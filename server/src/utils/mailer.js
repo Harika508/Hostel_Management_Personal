1910f10789bafb2d0b65995902c7aba1daa0a015
Harika508 <Harika.n@dramdynamics.com>
2026-07-06 00:06:21 +0530

    Fix email configuration

--- server/src/utils/mailer.js
+++ server/src/utils/mailer.js
@@ -2,12 +2,15 @@ const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
   host: process.env.MAIL_HOST,
-  port: parseInt(process.env.MAIL_PORT),
-  secure: false,
+  port: Number(process.env.MAIL_PORT),
+  secure: process.env.MAIL_PORT == 465,
   auth: {
     user: process.env.MAIL_USER,
     pass: process.env.MAIL_PASS,
   },
+  connectionTimeout: 30000,
+  greetingTimeout: 30000,
+  socketTimeout: 30000,
 });
 
 const sendPaymentReminder = async ({ to, studentName, amount, dueDate, type, daysBefore }) => {
