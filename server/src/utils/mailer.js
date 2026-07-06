68ac5edfc0ba4cdbb23a2980834ae1e89205d2fa
Harika508 <Harika.n@dramdynamics.com>
2026-07-06 13:44:14 +0530

    Use Gmail service for Nodemailer

--- server/src/utils/mailer.js
+++ server/src/utils/mailer.js
@@ -1,16 +1,11 @@
 const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
-  host: process.env.MAIL_HOST,
-  port: Number(process.env.MAIL_PORT),
-  secure: process.env.MAIL_PORT == 465,
+  service: "gmail",
   auth: {
     user: process.env.MAIL_USER,
     pass: process.env.MAIL_PASS,
   },
-  connectionTimeout: 30000,
-  greetingTimeout: 30000,
-  socketTimeout: 30000,
 });
 
 const sendPaymentReminder = async ({ to, studentName, amount, dueDate, type, daysBefore }) => {
